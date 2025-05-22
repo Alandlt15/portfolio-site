5278601f79b1341fa354ea11d9dbfa5ec81bd467
Alan De La Torre <sua9@txstate.edu>
2025-05-22 15:21:47 -0500

    added two cards to homepage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,6 +12,21 @@ function Home() {
   return (
     <>
       <Header/>
+      <article className='card_one'>
+        <div className='inner_card_one'>
+          <h2>Built on Trust</h2>
+          <p>
+            Let us take care of your website or mobile application needs!
+          </p>
+        </div>
+
+        <div className='inner_card_one'>
+          <h2>Built on Trust</h2>
+          <p>
+            Let us take care of your website or mobile application needs!
+          </p>
+        </div>
+      </article>
     </>
   )
 }
