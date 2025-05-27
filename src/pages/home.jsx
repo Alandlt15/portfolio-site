4b51b605e2c30dae076ef7cfd363ba17f0271d30
Alan <allan-1963@hotmail.com>
2025-05-26 19:28:42 -0500

    added html vs react comparison

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,6 +5,7 @@ import { useState } from 'react'
 import Header from '../components/header'
 import Footer from '../components/footer'
 import htmlpic from '../assets/html-example.jpg'
+import reactpic from '../assets/react-example.jpeg'
 
 //style import
 import '../styles/home.css'
@@ -16,22 +17,26 @@ function Home() {
       <Header/>
       <article className='card_one'>
         <div className='inner_card_one'>
-          <h2>Built on Trust</h2>
+          <h2>Creovant Serves Individuals and Local <br/>Companies</h2>
           <p>
-            Let us take care of your website or mobile application needs!
+            by providing web and mobile application services
           </p>
         </div>
 
         <div className='inner_card_one'>
-          <h2>Built on Trust</h2>
+          <h2>Affordable <br/> Community Oriented <br/> Modern Technologies</h2>
           <p>
-            Let us take care of your website or mobile application needs!
+            Something else here!
           </p>
         </div>
 
         <div className='inner_card_two'>
-          <h2>Take your website from this</h2>
+          <h2>We take your website from this</h2>
           <img src={htmlpic} alt="Image of a basic html page" className='img-styling'/>
+        </div>
+        <div className='inner_card_two'>
+          <h2>To this</h2>
+          <img src={reactpic} alt="Image of a basic html page" className='img-styling'/>
         </div>
       </article>
       <Footer/>
